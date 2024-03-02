359dd9219cebe841dd719a787d7274a9bc4c2d31
Thang Le <leducthang98@gmail.com>
2024-03-02 23:58:11 +0700

    ok

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ import { ResponseBuilder } from './ResponseBuilder';
 const data: ResponseBuilder<string> = ResponseBuilder.buildResponse('dataaa');
 
 console.log(data);
+console.log('abc', data);
 console.log(data);
